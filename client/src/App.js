d597e1b4a308df4fa1cd1d7cfaff07f4fe829ace
Israel M. Rios <israelm.riosjr@gmail.com>
2022-01-24 22:01:14 -0800

    added components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-     
+      <h1>Hello World</h1>
     </div>
   );
 }
