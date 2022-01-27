c8d06e4be165e4846c09ac3f322afbcfe1f3167c
Israel M. Rios <israelm.riosjr@gmail.com>
2022-01-26 21:01:28 -0800

    Added Header Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
   );
 }
 
