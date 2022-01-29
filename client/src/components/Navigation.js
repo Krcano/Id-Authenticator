4431dacece7468eb40cd03acff9b42fa8fe3a8d6
Israel M. Rios <israelm.riosjr@gmail.com>
2022-01-28 22:08:37 -0800

    Moved component to pages and styled landing page

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,74 +5,74 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   const [currentTab, setCurrentTab] = useState("");
   return (
-    <ul>
-      <li>
-        <Link
-          to="/"
-          className={currentTab === "/" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("/");
-          }}
-        >
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/testimonials"
-          className={currentTab === "testimonials" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("testimonials");
-          }}
-        >
-          Testimonials
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/login"
-          className={currentTab === "login" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("login");
-          }}
-        >
-          Log in
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/signUp"
-          className={currentTab === "signUp" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("signUp");
-          }}
-        >
-          Sign Up
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/profile"
-          className={currentTab === "profile" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("profile");
-          }}
-        >
-          Profile
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/contact"
-          className={currentTab === "contact" ? "active" : "navbarA"}
-          onClick={() => {
-            setCurrentTab("contact");
-          }}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+      <ul>
+        <li>
+          <Link
+            to="/"
+            className={currentTab === "/" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("/");
+            }}
+          >
+            HOME
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/testimonials"
+            className={currentTab === "testimonials" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("testimonials");
+            }}
+          >
+            TESTIMONIALS
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/login"
+            className={currentTab === "login" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("login");
+            }}
+          >
+            LOGIN
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/signUp"
+            className={currentTab === "signUp" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("signUp");
+            }}
+          >
+            SIGN-UP
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/profile"
+            className={currentTab === "profile" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("profile");
+            }}
+          >
+            PROFILE
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact"
+            className={currentTab === "contact" ? "active" : "navbarA"}
+            onClick={() => {
+              setCurrentTab("contact");
+            }}
+          >
+            CONTACT
+          </Link>
+        </li>
+      </ul>
   );
 };
 
