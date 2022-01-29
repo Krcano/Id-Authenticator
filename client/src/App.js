df5e4a359344d9b6e577cdd484c96d46c5811ece
jdesgarennes <johndesgarennes@gmail.com>
2022-01-29 13:19:42 -0800

    asdfsadf

--- client/src/App.js
+++ client/src/App.js
@@ -6,52 +6,25 @@ import React from "react";
 //   createHttpLink,
 // } from '@apollo/client';
 // import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import Footer from "./components/Footer";
-import { useEffect, useRef } from "react";
-import * as faceapi from "face-api.js";
+import Footer from './components/Footer'
 
 function App() {
-  const imgRef = useRef();
-  const canvasRef = useRef();
-  const hanldeImage = (async) => {
-    const detect = await faceapi
-      .detectAllFaces(imagRef.current, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
-
-    console.log(detect);
-  };
-
-  useEffect(() => {
-    const loadModels = () => {
-      Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-      ]).then(hanldeImage);
-      console.log("its done");
-    };
-
-    imgRef.current && loadModels;
-  });
   return (
     <Router>
       <>
         <Header />
         <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/" element={<Landing />}></Route>  
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/signup" element={<SignUp/>}></Route>          
         </Routes>
-        <Footer />
+        <Footer/>
       </>
-      <canvas ref={canvasRef} />
     </Router>
   );
 }
---
+++ client/src/components/Face.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import video from '../img/crowd-of-people-walking-on-nyc-sidewalk.mp4'
+
+const Face = () => {
+  return (
+  <header className='header'>
+        <div>
+            <img src='../img/woldo.png' alt='woldo icon' className='icon'></img>
+        </div>
+        <div className='nav'>
+            <Navigation />
+        </div>    
+  </header>
+  );
+};
+
+export default Face;
