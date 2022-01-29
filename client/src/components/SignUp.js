fce85603186332df1827efc49be016f0eee06d2c
Kelly R Cano <krcano2001@gmail.com>
2022-01-29 11:53:49 -0800

    added footer css

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -7,65 +7,6 @@ import { Link } from "react-router-dom";
 // download
 // import {useMutation} from '@apollo/client'
 
-const styles = {
-  background: {
-    background: "#222330",
-    height: "100vh",
-  },
-
-  input: {
-    display: "flex",
-    padding: " 14px 32px 14px 16px",
-    borderRadius: "4px",
-    outline: "none",
-    width: "400px",
-    fontFamily: "serif",
-    marginBottom: "20px",
-    fontSize: "1.25em",
-    color: "black",
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-
-    color: "white",
-    alignItems: "center",
-    fontFamily: "Playfair Display serif",
-  },
-  button: {
-    border: "none",
-    padding: "5px 10px",
-    // margin: "5px",
-    borderRadius: "5px",
-    cursor: "pointer",
-    /* text-decoration: none; */
-    fontSize: "1.5em",
-    fontFamily: "inherit",
-    width: "400px",
-    background: "#ff6162",
-    color: "white",
-    position: "absolute",
-  },
-  buttonContainer: {
-    display: "flex",
-    justifyContent: "center",
-  },
-  label: {
-    fontWeight: "bold",
-    fontFamily: "serif",
-    fontSize: " 1.5em",
-    paddingRight: "10px",
-  },
-  h1: {
-    display: "flex",
-    color: "white",
-    justifyContent: "center",
-  },
-  link: {
-    textDecorationColor: "white",
-    color: "white",
-  },
-};
 const SignUp = () => {
   const [FormData, setUserFormData] = useState({ email: "", password: "" });
   // will need to bring in login mutation and name it ADD_USER
@@ -93,25 +34,25 @@ const SignUp = () => {
   //   };
 
   return (
-    <div id="signup" style={styles.background}>
-      <h1 style={styles.h1}>Sign Up</h1>
+    <div id="signup" className="background">
+      <h1 className="h1">Sign Up</h1>
       <ParticlesBackground />
-      <Link style={styles.link} to="/login">
+      <Link className="link" to="/login">
         {" "}
-        <h2 style={styles.link}>← Go to Login</h2>
+        <h2 className="link">← Go to Login</h2>
       </Link>
 
       {/* referenced redux store hw */}
       <form
-        style={styles.form}
+        className="form"
         //    onSubmit={handleFormSubmit}
       >
         <div>
-          <label style={styles.label} htmlFor="firstName">
+          <label className="label" htmlFor="firstName">
             First Name:
           </label>
           <input
-            style={styles.input}
+            className="input"
             placeholder="First"
             name="firstName"
             type="firstName"
@@ -120,11 +61,11 @@ const SignUp = () => {
           />
         </div>
         <div>
-          <label style={styles.label} htmlFor="lastName">
+          <label className="label" htmlFor="lastName">
             Last Name:
           </label>
           <input
-            style={styles.input}
+            className="input"
             placeholder="Last"
             name="lastName"
             type="lastName"
@@ -132,12 +73,12 @@ const SignUp = () => {
             onChange={handleInputChange}
           />
         </div>
-        <div style={styles.emailContainer} className="">
-          <label style={styles.label} htmlFor="email">
+        <div className="emailContainer">
+          <label className="label" htmlFor="email">
             Email address:
           </label>
           <input
-            style={styles.input}
+            className="input"
             placeholder="youremail@test.com"
             name="email"
             type="email"
@@ -146,11 +87,11 @@ const SignUp = () => {
           />
         </div>
         <div className="">
-          <label style={styles.label} htmlFor="pwd">
+          <label className="label" htmlFor="pwd">
             Password:
           </label>
           <input
-            style={styles.input}
+            className="input"
             placeholder="******"
             name="password"
             type="password"
@@ -158,9 +99,9 @@ const SignUp = () => {
             onChange={handleInputChange}
           />
         </div>
-        
-        <div style={styles.buttonContainer} className="">
-          <button style={styles.button} type="submit">
+
+        <div className="buttonContainer">
+          <button className="button" type="submit">
             Submit
           </button>
         </div>
